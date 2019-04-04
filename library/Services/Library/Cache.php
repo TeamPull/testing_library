71c8825141cd36d90571c804873c32f00818f54a
hkreuter <heike@reuter-consultig.org>
2019-04-04 14:14:00 +0200

    OXDEV-1706 Add nginx cache clear to service

--- library/Services/Library/Cache.php
+++ library/Services/Library/Cache.php
@@ -38,6 +38,7 @@ class Cache
             $oReverseProxy->setFlush();
             $oReverseProxy->execute();
         }
+        $this->clearNginxProxyCache();
     }
 
     /**
@@ -74,6 +75,36 @@ class Cache
         }
         if ($rmBaseDir) {
             @rmdir($dir);
+        }
+    }
+
+    /**
+     * Test helper to clear nginx cache
+     */
+    protected function clearNginxProxyCache()
+    {
+        $facts = new \OxidEsales\Facts\Facts();
+        if (!$facts->isEnterprise()) {
+            return;
+        }
+
+        $testModulePaths = $this->getTestConfig()->getPartialModulePaths();
+        if (in_array('oe/nginx', $testModulePaths)) {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $this->getTestConfig()->getShopUrl() . 'nginx-clear-cache');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 31);
+            curl_setopt($ch, CURLOPT_HTTPGET, 1);
+            curl_setopt($ch, CURLOPT_USERAGENT, "OXID-TEST");
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, "all=true");
+            $result = curl_exec($ch);
+
+            $databaseMetaDataHandler = oxNew(\OxidEsales\Eshop\Core\DbMetaDataHandler::class);
+            if ($databaseMetaDataHandler->tableExists(\OxidEsales\NginxModule\Cache\Backend::NGINX_HASH_TABLE)) {
+                $query = 'TRUNCATE table ' . \OxidEsales\NginxModule\Cache\Backend::NGINX_HASH_TABLE;
+                \OxidEsales\Eshop\Core\DatabaseProvider::getDb()->execute($query);
+            }
         }
     }
 }
